9a4875393c0245ae5ee2e3fd58bd487cf823f59c
Ömer Cesur <96197600+OmerCesur@users.noreply.github.com>
2024-02-10 21:31:23 +0300

    Unnecessary files removed

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 
 # Importing the libraries
 import numpy as np
-from random import random, randint
 import matplotlib.pyplot as plt
 import time
 
@@ -10,7 +9,7 @@ import time
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
-from kivy.graphics import Color, Ellipse, Line
+from kivy.graphics import Color, Line
 from kivy.config import Config
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
